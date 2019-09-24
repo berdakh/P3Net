a17da3041a7f08012a7b12c22235316b502c07f2
Berdakh Abibullaev <48582495+berdakh@users.noreply.github.com>
2019-09-24 14:26:13 +0600

    Add files via upload

---
+++ datasets-download.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 14:19:18 2019
+@author: berdakh
+
+We share the datasets used in the paper via the links shown below. 
+
+Most datasets (except NU data), are available through MOABB toolbox (https://github.com/NeuroTechX/moabb). 
+Also note that we have converted all the provided data to MNE format for easier processing and visualization.
+
+You can download them and save in the local directory where the source codes will ba available. 
+
+NU data: 
+    https://drive.google.com/open?id=19SwPDCnZ1zL9G5QCieA_ZrWXPXMtN24Y
+ALS data: 
+    https://drive.google.com/open?id=1frdRrw1WGT0y9hfdQ9XSwXjlwor6OKD2 
+EPFL data: 
+    https://drive.google.com/open?id=19py9fTIE7ylW_pXH6ycByu5KjuDS9T-N
+BNCI data: 
+    https://drive.google.com/open?id=1V-teLkK2XWFR4AFf2hA6TV1IsHdSBVLq
+    
+---------------------------------------------------------------------- 
+Independent data: 
+    https://drive.google.com/open?id=1m4_HbBwNQhxzsImyHnJFZAxYCEP1dyIW
+"""
+
